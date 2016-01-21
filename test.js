c3474fafa6cce0d027b14f900b739695f1e6da21
Matt Bell <mappum@gmail.com>
2016-01-20 17:42:48 -0800

    Added more tests

--- test.js
+++ test.js
@@ -1,29 +1,69 @@
 var test = require('tap').test
 var BN = require('bn.js')
+var buffertools = require('buffertools')
 var u = require('.')
 
 test('toHash', function (t) {
-  var actual = u.toHash('0123456789')
-  var expected = new Buffer([ 0x89, 0x67, 0x45, 0x23, 0x01 ])
+  t.throws(function () { u.toHash('012345') }, 'throws for invalid hash length')
+  t.throws(function () { u.toHash('') }, 'throws for empty string')
+  var actual = u.toHash('ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff00')
+  var expected = new Buffer([
+    0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255
+  ])
   t.equal(actual.compare(expected), 0)
   t.end()
 })
 
 test('toCompactTarget', function (t) {
-  var target = new BN('ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff', 'hex')
-  t.equal(u.toCompactTarget(target), 0x2200ffff)
-
-  target = new BN('ffff0000000000000000000000000000000000000000000000000000', 'hex')
-  t.equal(u.toCompactTarget(target), 0x1d00ffff)
-
-  target = new BN('ffff0000000000000000000000000000000000000000000000000001', 'hex')
-  t.equal(u.toCompactTarget(target), 0x1d00ffff)
-
-  target = new BN('7fff0000000000000000000000000000000000000000000000000000', 'hex')
-  t.equal(u.toCompactTarget(target), 0x1c7fff00)
-
-  target = new BN('0', 'hex')
-  t.equal(u.toCompactTarget(target), 0x0)
-
+  var targets = [
+    {
+      hex: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+      compact: 0x2200ffff
+    },
+    {
+      hex: 'ffff0000000000000000000000000000000000000000000000000000',
+      compact: 0x1d00ffff
+    },
+    {
+      hex: 'ffff0000000000000000000000000000000000000000000000000001',
+      compact: 0x1d00ffff
+    },
+    {
+      hex: '7fff0000000000000000000000000000000000000000000000000000',
+      compact: 0x1c7fff00
+    },
+    {
+      hex: '00',
+      compact: 0
+    }
+  ]
+  t.test('bn.js', function (t) {
+    targets.forEach(function (target) {
+      var bn = new BN(target.hex, 'hex')
+      t.equal(u.toCompactTarget(bn), target.compact, target)
+    })
+    t.end()
+  })
+  t.test('Buffer', function (t) {
+    targets.forEach(function (target) {
+      var buf = new Buffer(target.hex, 'hex')
+      t.equal(u.toCompactTarget(buf), target.compact, target)
+    })
+    t.end()
+  })
+  t.test('hex string', function (t) {
+    targets.forEach(function (target) {
+      var reversed = new Buffer(target.hex, 'hex')
+      reversed = buffertools.reverse(reversed).toString('hex')
+      t.equal(u.toCompactTarget(reversed), target.compact, target)
+    })
+    t.end()
+  })
+  t.test('invalid type', function (t) {
+    t.throws(function () {
+      u.toCompactTarget(123)
+    })
+    t.end()
+  })
   t.end()
 })
